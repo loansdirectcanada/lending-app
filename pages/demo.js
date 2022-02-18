0bf293f3c70a9b0e3bd6d78a5278a737373807c2
anirban00537 <anirban00537@gmail.com>
2022-02-18 22:25:29 +0600

    update

---
+++ pages/demo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const demo = () => {
+  return (
+    <div>
+      <img src="/1.jpg" alt="Lights" style={{ width: "100%" }} />
+      <img src="/2.jpg" alt="Lights" style={{ width: "100%" }} />
+      <img src="/3.jpg" alt="Lights" style={{ width: "100%" }} />
+    </div>
+  );
+};
+
+export default demo;
