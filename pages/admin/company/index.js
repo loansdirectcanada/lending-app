7cf7a87261b321a202244521509f30d3d1dcf1ee
anirban00537 <anirban00537@gmail.com>
2022-03-22 22:34:25 +0600

    updated

--- pages/admin/company/index.js
+++ pages/admin/company/index.js
@@ -13,6 +13,7 @@ import {
   Heading,
   Text,
   useColorModeValue,
+  Button,
 } from "@chakra-ui/react";
 import Drawer from "../../../components/admin/drawerCompany";
 import db from "../../../firebase";
@@ -22,13 +23,31 @@ import AdminNav from "../../../components/adminNavbar";
 const Lending = () => {
   const [AllCompanies, setAllCompanies] = React.useState([]);
   const [business, setBusiness] = React.useState({});
+  const [dependency, setDependency] = React.useState(5);
   const getAllLoansInfo = async () => {
     const loans = await db.collection("company_loans_applications").get();
     setAllCompanies(loans.docs.map((doc) => doc.data()));
   };
+  const deleteBusinessById = (id) => {
+    db.collection("company_loans_applications")
+      .where("id", "==", id)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          doc.ref.delete();
+        });
+      })
+      .then(async () => {
+        const loans = await db.collection("company_loans_applications").get();
+        console.log(loans.docs.map((doc) => doc.data()));
+        setAllCompanies(loans.docs.map((doc) => doc.data()));
+        const random = Math.floor(Math.random() * 100);
+        setDependency(random);
+      });
+  };
   useEffect(() => {
     getAllLoansInfo();
-  }, []);
+  }, [dependency]);
   return (
     <ChakraProvider>
       <AdminNav />
@@ -45,9 +64,12 @@ const Lending = () => {
       </Text>
 
       {AllCompanies.length === 0 ? (
-        <Box height={["100vh", "100vh", "100vh", "100vh"]}></Box>
+        <Box
+          height={["100vh", "100vh", "100vh", "100vh"]}
+          minHeight="100vh"
+        ></Box>
       ) : (
-        <Box height={["100%", "100%", "100%", "100%"]}>
+        <Box height={["100%", "100%", "100%", "100%"]} minHeight="100vh">
           <Table variant="striped">
             <Thead>
               <Tr>
@@ -71,6 +93,19 @@ const Lending = () => {
                     }}
                   >
                     <Drawer business={business} />
+                  </Td>
+                  <Td
+                    onClick={() => {
+                      deleteBusinessById(Company.id);
+                    }}
+                  >
+                    <Button
+                      variantColor="red"
+                      color={useColorModeValue("gray.500", "gray.300")}
+                      fontSize="sm"
+                    >
+                      Delete
+                    </Button>
                   </Td>
                 </Tr>
               ))}
--- pages/admin/lending/index.js
+++ pages/admin/lending/index.js
@@ -13,6 +13,7 @@ import {
   Heading,
   Text,
   useColorModeValue,
+  Button,
 } from "@chakra-ui/react";
 import Drawer from "../../../components/admin/drawer";
 import db from "../../../firebase";
@@ -22,13 +23,33 @@ import AdminNav from "../../../components/adminNavbar";
 const Lending = () => {
   const [Allloans, setAllloans] = React.useState([]);
   const [business, setBusiness] = React.useState({});
+  const [dependency, setDependency] = React.useState(5);
   const getAllLoansInfo = async () => {
     const loans = await db.collection("loans_applications").get();
     setAllloans(loans.docs.map((doc) => doc.data()));
   };
+
+  const deleteLonaById = (id) => {
+    console.log(id, "ididid");
+    db.collection("loans_applications")
+      .where("id", "==", id)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          doc.ref.delete();
+        });
+      })
+      .then(async () => {
+        const loans = await db.collection("loans_applications").get();
+        console.log(loans.docs.map((doc) => doc.data()));
+        setAllloans(loans.docs.map((doc) => doc.data()));
+        const random = Math.floor(Math.random() * 100);
+        setDependency(random);
+      });
+  };
   useEffect(() => {
     getAllLoansInfo();
-  }, []);
+  }, [dependency]);
   return (
     <ChakraProvider>
       <AdminNav />
@@ -43,9 +64,12 @@ const Lending = () => {
         Lending application
       </Text>
       {Allloans.length === 0 ? (
-        <Box height={["100vh", "100vh", "100vh", "100vh"]}></Box>
+        <Box
+          height={["100vh", "100vh", "100vh", "100vh"]}
+          minHeight="100vh"
+        ></Box>
       ) : (
-        <Box height={["100%", "100%", "100%", "100%"]}>
+        <Box height={["100%", "100%", "100%", "100%"]} minHeight="100vh">
           <Table variant="striped">
             <Thead>
               <Tr>
@@ -69,6 +93,14 @@ const Lending = () => {
                     }}
                   >
                     <Drawer business={business} />
+                  </Td>
+                  <Td>
+                    <Button
+                      variantColor="red"
+                      onClick={() => deleteLonaById(loan.id)}
+                    >
+                      Delete
+                    </Button>
                   </Td>
                 </Tr>
               ))}
